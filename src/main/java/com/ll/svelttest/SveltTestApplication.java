acb5c8fd1ab205b84882f6c2140667f7801560c0
BuKwon Kim <dnu05043@naver.com>
2024-01-10 11:14:49 +0900

    start

---
+++ src/main/java/com/ll/svelttest/SveltTestApplication.java
@@ -0,0 +1,13 @@
+package com.ll.svelttest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SveltTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SveltTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/svelttest/SveltTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.svelttest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SveltTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
